23a1d215634bdfec4e063c86c045959fcb5940fd
tkacalex <alexey.mail.gma@gmail.com>
2021-04-13 18:19:58 +0200

    lesson #2

--- lesson2.py
+++ lesson2.py
@@ -1,13 +1,9 @@
 a = [3, 4, 2, 3, 5, 8, 23, 32, 35, 34, 4, 6, 9]
 
+print("")
+print("Lesson #2")
+print("Program start:")
 for i in a:
-  print(i)
-  if i != 8:
-    continue
-  break
-
-b = [4, 6, 9]
-for f in b:
-  print(f)
-  if f == 8:
-    break
+  if i < 9:
+    print(i)
+print("End")
