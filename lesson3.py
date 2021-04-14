b237d530447445871c4b01e55eee5f20744fccc3
tkacalex <alexey.mail.gma@gmail.com>
2021-04-14 14:45:22 +0200

    lesson #3

---
+++ lesson3.py
@@ -0,0 +1,11 @@
+a = [3, 4, 2, 3, 5, 8, 9, 23, 32, 35, 34, 4, 6, 9]
+b = [3, 4, 2, 3, 5, 8, 9, 23, 32, 35, 34, 4, 6, 9]
+
+print("")
+print("Lesson #3")
+print("Program start:")
+a.sort()
+print(a)
+b = sorted(b, reverse=True)
+print (b)
+print("End")
